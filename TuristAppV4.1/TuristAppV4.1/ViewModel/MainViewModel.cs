a77c21d8dae5ce43afd6244a253fb49056986b06
Daniel <danielwinther@hotmail.dk>
2014-11-17 17:05:03 +0100

    benjamin branch

--- TuristAppV4.1/TuristAppV4.1/ViewModel/MainViewModel.cs
+++ TuristAppV4.1/TuristAppV4.1/ViewModel/MainViewModel.cs
@@ -1,25 +1,60 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TuristAppV4._1.Annotations;
 using TuristAppV4._1.Model;
 
 namespace TuristAppV4._1.ViewModel
 {
-    internal class MainViewModel
+    internal class MainViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants1 { get; set; } //lav til backing field
-        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants2 { get; set; } //same
-        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants3 { get; set; } //same
-        private Restaurant _selectedRestaurant;
+     private Restaurant _selectedRestaurant;
+        private ObservableCollection<Restaurant> _observableCollectionOfRestuarants1;
+        private ObservableCollection<Restaurant> _observableCollectionOfRestuarants2;
+        private ObservableCollection<Restaurant> _observableCollectionOfRestuarants3;
 
+        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants1
+        {
+            get { return _observableCollectionOfRestuarants1; }
+            set { _observableCollectionOfRestuarants1 = value; }
+        }
+
+        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants2
+        {
+            get { return _observableCollectionOfRestuarants2; }
+            set { _observableCollectionOfRestuarants2 = value; }
+        }
+
+        public ObservableCollection<Restaurant> ObservableCollectionOfRestuarants3
+        {
+            get { return _observableCollectionOfRestuarants3; }
+            set { _observableCollectionOfRestuarants3 = value; }
+        } 
         public Restaurant SelectedRestaurant
         {
             get { return _selectedRestaurant; }
-            set
-            {
-                _selectedRestaurant = value; //OnNotifyPropertyChanged()
-            }
+            set { _selectedRestaurant = value; OnPropertyChanged(); }
         }
 
-        public void SletRestaurant() { }
+        public void SletRestaurant()
+        {
+            if (SelectedRestaurant == null) { }
+            else
+            {
+                if (ObservableCollectionOfRestuarants1.Contains(SelectedRestaurant))
+                {
+                    ObservableCollectionOfRestuarants1.Remove(SelectedRestaurant);
+                }
+                else if (ObservableCollectionOfRestuarants2.Contains(SelectedRestaurant))
+                {
+                    ObservableCollectionOfRestuarants2.Remove(SelectedRestaurant);
+                }
+                else
+                {
+                    ObservableCollectionOfRestuarants3.Remove(SelectedRestaurant);
+                }         
+             }
+        }
 
         public MainViewModel()
         {
@@ -30,7 +65,19 @@ namespace TuristAppV4._1.ViewModel
             ObservableCollectionOfRestuarants2.Add(new Restaurant("testNavn", "testBedømmelse", "testHjemmeside", "testBeskrivelse", "testTelefon"));
             ObservableCollectionOfRestuarants3.Add(new Restaurant("testNavn", "testBedømmelse", "testHjemmeside", "testBeskrivelse", "testTelefon"));
         }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+       
+        #endregion
     }
-
-
 }
+
--- TuristAppV4.1/TuristAppV4.1/ViewModel/OpretRestuarantHandler.cs
+++ TuristAppV4.1/TuristAppV4.1/ViewModel/OpretRestuarantHandler.cs
@@ -5,20 +5,27 @@ namespace TuristAppV4._1.ViewModel
 {
     class OpretRestuarantHandler
     {
-        private string _restaurantNavn;
-        private string _bedømmelse;
-        private string _hjemmeside;
-        private string _beskrivelse;
-        private string _telefon;
+        private string _restaurantNavn; // twoway bindes til textbox
+        private string _bedømmelse;  // twoway bindes til textbox
+        private string _hjemmeside;   // twoway bindes til textbox
+        private string _beskrivelse;   // twoway bindes til textbox
+        private string _telefon;   // twoway bindes til textbox
         private MainViewModel _reference = new MainViewModel(); //reference til collection
 
 
-        public void OpretRestaurant(ObservableCollection<Restaurant> observableCollection)
+        public void OpretRestaurantKategori1(ObservableCollection<Restaurant> observableCollection)
         {
-
             _reference.ObservableCollectionOfRestuarants1.Add(new Restaurant(_restaurantNavn, _bedømmelse, _hjemmeside, _beskrivelse, _telefon));
-            // problem med at den tilføjer en restaurant til alle tre OC's
-            // kan ikke vælge specifik OC
+        }
+
+        public void OpretRestaurantKategori2(ObservableCollection<Restaurant> observableCollection)
+        {
+            _reference.ObservableCollectionOfRestuarants2.Add(new Restaurant(_restaurantNavn, _bedømmelse, _hjemmeside, _beskrivelse, _telefon));
+        }
+
+        public void OpretRestaurantKategori3(ObservableCollection<Restaurant> observableCollection)
+        {
+            _reference.ObservableCollectionOfRestuarants3.Add(new Restaurant(_restaurantNavn, _bedømmelse, _hjemmeside, _beskrivelse, _telefon));
         }
     }
 }
